9878a8be27cce77aec5fe80937baf7d93fa8c8f3
ugis <berzinsu@gmail.com>
2019-01-31 17:15:27 +0200

    multiple inquiry validation and email configuration

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
 
 def contact(request):
     if request.method == 'POST':
@@ -13,10 +14,26 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        #check if user has made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id 
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing_id=listing_id,listing=listing,name=name,email=email,phone=phone,message=message,
         user_id=user_id)
 
         contact.save()
+
+        send_mail(
+            'Property Listing Inquiry',
+            'There has beed an inquiry for ' + listing + '. Sign into the admin panel for more info',
+            'oy@mate.com',
+            [realtor_email, 'techsupporty@gmail.com'],
+            fail_silently=True
+        )
 
         messages.success (request, 'Your request has been submitted, a realtor will get back to you')
 
