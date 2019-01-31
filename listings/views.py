39018b67b506c99ee5cd4adbf65ddc9d9c7cde72
ugis <berzinsu@gmail.com>
2019-01-31 14:04:47 +0200

    Search choices made dynamic and search page template updated

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
+from .choices import *
+
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
@@ -23,4 +25,13 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html',context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+        'state_choices' : state_choices,
+        'price_choices' : price_choices,
+        'bedroom_choices' : bedroom_choices,
+    }
+
+    return render(request, 'listings/search.html',context)
