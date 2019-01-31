6bfb711d5100c18cd90eedd86447cfdad0577ba0
ugis <berzinsu@gmail.com>
2019-01-31 14:34:16 +0200

    Search form retains values

--- listings/views.py
+++ listings/views.py
@@ -61,6 +61,7 @@ def search(request):
         'state_choices' : state_choices,
         'price_choices' : price_choices,
         'bedroom_choices' : bedroom_choices,
+        'values' : request.GET
     }
 
     return render(request, 'listings/search.html',context)
